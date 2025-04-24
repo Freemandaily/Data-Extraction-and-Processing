15eec50befa844a251867c39702831e347444b37
Freeman <107298762+Freemandaily@users.noreply.github.com>
2025-04-24 19:21:28 +0100

    Create test.py

---
+++ test.py
@@ -0,0 +1,14 @@
+import requests
+
+moralis = "eyJub25jZSI6IjBmZjJiMGYxLTE4ZDctNDFkMi05OGM1LTllZmYxOTNmZDJmZSIsIm9yZ0lkIjoiMzU3MTg0IiwidXNlcklkIjoiMzY3MDk3IiwidHlwZSI6IlBST0pFQ1QiLCJ0eXBlSWQiOiJmYWQ3ZWQ2ZC0wODk0LTRiZjUtODBhNS0zZTQwYzZkMDZkODciLCJpYXQiOjE2OTQ3MTk5MDYsImV4cCI6NDg1MDQ3OTkwNn0"
+
+
+url = f"https://solana-gateway.moralis.io/token/mainnet/pairs/{pair}/ohlcv?timeframe=5min&currency=usd&fromDate={from_date}&toDate={to_date}&limit=1000"
+
+    headers = {
+    "Accept": "application/json",
+    "X-API-Key": f"{moralis}"
+    }
+  
+response = requests.request("GET", url, headers=headers)
+st.write(response.status_code)
